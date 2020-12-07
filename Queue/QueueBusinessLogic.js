679778b30f9af2bfeec4767bde5a652adf9358d7
Akshay <bharambeakshay1997@gmail.com>
2020-12-08 00:36:27 +0530

    [Akshay] Added . Queue business logic  enque and dqueue

---
+++ Queue/QueueBusinessLogic.js
@@ -0,0 +1,65 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+    peek() {
+        // This method shows first node in queue
+        if (!this.first) {
+            return null;
+        } else {
+            return this.first;
+        }
+
+
+    }
+
+    enqueue(data) {
+        //If there is no first, it adds to new first, else element get back of the line
+        let node = new Node();
+        if (!this.first) {
+            this.first = node;
+            this.last = node;
+            this.length++;
+
+        } else {
+            this.last.next = node
+            this.last = node;
+            this.length++;
+        }
+        return this;
+
+
+    }
+    dequeue() {
+        // first elemnet get off the  queue
+        if (this.first === this.last) {
+            const tackingNode = this.first;
+            this.first = null;
+            this.last = null
+            this.length--;
+        } else {
+            const trackingNode = this.first;
+            this.first = this.first.next;
+            this.length--;
+        }
+        return trackingNode
+    }
+}
+
+let q = new Queue();
+q.enqueue(10);
+q.enqueue(20);
+q.enqueue(30);
+q.enqueue(40);
+q.enqueue(50);
+q.dequeue()
+console.log(q);
