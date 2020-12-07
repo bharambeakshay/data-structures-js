d0e99bc16eeb7a6ffcc813e2bafec4f6eb2baca4
Akshay <bharambeakshay1997@gmail.com>
2020-12-07 14:08:29 +0530

    [Akshay] Refactor. Removal of undefined variables, line spacing and prompt

--- Stack/StackMain.js
+++ Stack/StackMain.js
@@ -1,8 +1,5 @@
-var readline = require('readline-sync');
-const prompt = require('prompt-sync')();
+const readline = require('readline-sync');
 const Stack = require('./StackBusinessLogic');
-
-
 
 //flag is taken to stop the program. i.e to come out of infinite loop
 let flag = true;
@@ -12,15 +9,13 @@ let stack = new Stack();
 
 while (flag) {
 
-    //userunput to select the option in menu    
-    let userInput = readline.question("enter choice 1.push 2.pop 3.display 4. exit");
-    console.log(userInput);
-
+    //userinput to select the option in menu    
+    let userInput = readline.question(`enter choice  1.push  2.pop   3.display   4. exit: `);
     switch (userInput) {
 
         case "1":
             //length of stack asks user to input stack size
-            var lengthOfStack = Number(prompt("what is size of stack: "));
+            let lengthOfStack = readline.questionInt("what is size of stack: ");
 
             if (stack.sizee() <= lengthOfStack) {
                 for (let i = 0; i < lengthOfStack; i++) {
@@ -41,9 +36,9 @@ while (flag) {
             break;
 
         case "4":
-        //flag = false;
-        //process.exit(1);
-        //break;
+            flag = false;
+            process.exit(1);
+            break;
         default:
             console.log("Provide valid option");
             break;
