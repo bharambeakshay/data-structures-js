a7e3d66da4dec79bd6abfc7082dcdc7072a0b6e3
Akshay <bharambeakshay1997@gmail.com>
2020-12-09 01:08:27 +0530

    [Akshay] Refactor . code is refactored as per javascript standards

--- OrderedList1/OrderedListMain.js
+++ OrderedList1/OrderedListMain.js
@@ -3,5 +3,5 @@ const linkedList = require('./OrderedListBusinessLogic');
 const readline = require("readline-sync");
 let filedata = fs.readFileSync('./OrderedList.txt');
 console.log("Numbers data read from file: " + filedata);
-searchNumber = readline.question("Enter a number to search");
+searchNumber = readline.question("Enter a number to search: ");
 linkedList.orderedList(filedata, searchNumber);     
